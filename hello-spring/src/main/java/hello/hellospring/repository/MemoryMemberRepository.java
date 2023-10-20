0ca44125668640924866718dd15ca0fe8c043273
codevarty <hisu7789@g.yju.ac.kr>
2023-10-20 17:28:18 +0900

    [fix] modify directory structure

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,4 +1,4 @@
-package hello.hellospring;
+package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
   private static Map<Long, Member> store = new HashMap<>();
-  private static long sequence = 0L;
+  private static long sequence = 0L; //primary key
   @Override
   public Member save(Member member) {
     member.setId(++sequence);
