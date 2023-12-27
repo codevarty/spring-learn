109832fd46dec65196dc7bdd78c6596255a3777b
codevarty <hisu7789@g.yju.ac.kr>
2023-12-27 17:21:47 +0900

    study bean scope

---
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,38 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+// 값이 두 개 이상 들어가는 경우 parameter를 명시해 준다.
+// 해당 클래스를 proxy로 만든다. -> provider와 같은 동작을 하게 된다.
+// 요청이 올 때 까지 조회 지연한다.
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close: " + this);
+    }
+}
